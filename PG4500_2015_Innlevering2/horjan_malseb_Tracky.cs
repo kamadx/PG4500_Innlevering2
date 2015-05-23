38c1e0585678764cdfa637afa74da1ae60d7bad5
kamadx <horjan13@student.westerdals.no>
2015-05-23 12:17:24 +0200

    Goes to (25,25) as specified, Then waits.

---
+++ PG4500_2015_Innlevering2/horjan_malseb_Tracky.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robocode;
+using Robocode.Util;
+
+
+namespace PG4500_2015_Innlevering2
+{
+	public class horjan_malseb_Tracky : AdvancedRobot
+	{
+		/*
+		 TODO: A* Algorithm
+		 TODO: Draw path
+		 TODO: Traverse path
+		 TODO: Win
+		 */
+
+		public override void Run()
+		{
+			//Startup - Go to (25, 25) and wait.
+			GoToPoint(25.0, 25.0);
+			Out.Write("#{0}\t{1}\n", Time, "Heading to (25, 25).");
+			WaitFor(new MoveCompleteCondition(this));
+			Out.Write("#{0}\t{1}\n", Time, "Arrived at (" + X.ToString("F") + "," + Y.ToString("F") + ").");
+			//SetTurnRight(180);
+
+
+			//Main Loop
+
+		}
+
+		//Instructs the robot to move to a specific place.
+		public void GoToPoint(double pointX, double pointY)
+		{
+			//Go to point specified
+			pointX -= X;
+			pointY -= Y;
+
+			double distance = Math.Sqrt(Math.Pow(pointX, 2) + Math.Pow(pointY, 2));
+			double angle = Utils.NormalRelativeAngle(Math.Atan2(pointX, pointY) - HeadingRadians);
+
+			double turnAngle = Math.Atan(Math.Tan(angle));
+			SetTurnRightRadians(turnAngle);
+			WaitFor(new TurnCompleteCondition(this));
+			SetAhead(distance * (angle == turnAngle ? 1 : -1));
+			Execute();
+		}
+
+
+	}
+}
