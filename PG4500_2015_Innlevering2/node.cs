0ec0e1f5ac5f155637461c1bb2bf5b72fa0f3b79
Zebezd <zebbom@hotmail.com>
2015-06-09 20:55:18 +0200

    implementation changes to Node.

--- PG4500_2015_Innlevering2/node.cs
+++ PG4500_2015_Innlevering2/node.cs
@@ -18,7 +18,7 @@ namespace PG4500_2015_Innlevering2
 		public int Sector { get { return _sector; } set { _sector = value; } }
 		public bool Visited { get { return _visited; } set { _visited = value; } }
 
-		public Node(bool walkable = false, int cost = 0, bool visited = false, int sector = 0)
+		public Node(bool walkable = false, int cost = 1, bool visited = false, int sector = 0)
 		{
 			Walkable = walkable;
 			Cost = cost;
@@ -31,15 +31,17 @@ namespace PG4500_2015_Innlevering2
 			return n.Walkable;
 		}
 
+		//implementation specific methods
+
+		//for printing the map back the way it's made.
 		public static implicit operator int(Node n)
 		{
-			return n.Cost;
+			return n.Walkable? 0: 1;
 		}
 
-		//implementation specific
 		public static implicit operator Node(int i)
 		{
-			return new Node(i == 0, i);
+			return new Node(i == 0);
 		}
 	}
 }
