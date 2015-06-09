f0273ff202606545339ec7200333d6b27e4a69cc
Zebezd <zebbom@hotmail.com>
2015-06-09 17:39:50 +0200

    cost and sector in Node

--- PG4500_2015_Innlevering2/node.cs
+++ PG4500_2015_Innlevering2/node.cs
@@ -10,20 +10,17 @@ namespace PG4500_2015_Innlevering2
 	class Node
 	{
 		private bool _walkable;
-		public bool Walkable
-		{
-			get
-			{
-				return _walkable;
-			}
-			set
-			{
-				_walkable = value;
-			}
-		}
+		private int _cost;
+		private int _sector;
+		public bool Walkable { get { return _walkable; } set { _walkable = value; } }
+		public int Cost { get { return _cost; } set { _cost = value; } }
+		public int Sector { get { return _sector; } set { _sector = value; } }
+
 		public Node(bool walkable = false, int cost = 0, int sector = 0)
 		{
 			Walkable = walkable;
+			Cost = cost;
+			Sector = sector;
 		}
 
 		public static implicit operator Node(bool b)
