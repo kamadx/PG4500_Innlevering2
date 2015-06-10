b830bfc33ed651506afbf6d7e12695b17fcaf8ff
Zebezd <zebbom@hotmail.com>
2015-06-10 15:19:23 +0200

    added Vector2

---
+++ PG4500_2015_Innlevering2/Vector2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roboUtil
+{
+	class Vector2
+	{
+		private double _x;
+		private double _y;
+		public double X { get { return _x; } set { _x = value; } }
+		public double Y { get { return _y; } set { _y = value; } }
+
+		public Vector2(double x, double y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public static Vector2 operator -(Vector2 v1, Vector2 v2)
+		{
+			return new Vector2(v1.X - v2.X, v1.Y - v2.Y);
+		}
+		public static Vector2 operator +(Vector2 v1, Vector2 v2)
+		{
+			return new Vector2(v1.X + v2.X, v1.Y + v2.Y);
+		}
+
+		public static Vector2 operator *(Vector2 v, double d)
+		{
+			return new Vector2(v.X * d, v.Y * d);
+		}
+
+		public static Vector2 operator /(Vector2 v, double d)
+		{
+			return new Vector2(v.X / d, v.Y / d);
+		}
+	}
+}
